0b24f9784ae1ae89996da590d9c3122e3c6413cd
gingerly-ui <111126422+gingerly-ui@users.noreply.github.com>
2023-01-12 00:11:03 +0200

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,17 +4,16 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-        """Represent a rectangle using BaseGeometry."""
+    """Represent a rectangle using BaseGeometry."""
 
-            def __init__(self, width, height):
-                        """Intialize a new Rectangle.
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
 
-                                Args:
-                                            width (int): The width of the new Rectangle.
-                                                        height (int): The height of the new Rectangle.
-                                                                """
-                                                                        self.integer_validator("width", width)
-                                                                                self.__width = width
-                                                                                        self.integer_validator("height", height)
-                                                                                                self.__height = height
-
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
